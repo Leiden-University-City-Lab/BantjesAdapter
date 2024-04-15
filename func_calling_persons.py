36d435110c5a161890ad80cbe93e0bad29ae0936
Z. Abedi <z.abedi@umail.leidenuniv.nl>
2024-04-15 14:48:17 +0200

    Update func_calling_persons.py
    func_calling_persons

--- func_calling_persons.py
+++ func_calling_persons.py
@@ -35,101 +35,171 @@ from langchain.pydantic_v1 import BaseModel, Field
 import enum
 from typing import List, Optional
 
-class Kinderen(BaseModel):
-    """Identifying information about a person's child."""
 
-    name: str = Field(..., description="Name of the child mentioned")
-    extra_info: str = Field(..., description="Extra info about that specific child such as:birth/death date/place, education and job.")
+class Kind(BaseModel):
+    """Identifying information about a person."""
 
-
-class PersonInfo(BaseModel):
-    """Information about a person."""
-    name: str = Field(..., description="Name of the person. each text can only contain one name")
-    geb: str = Field(..., description="The birth date of the peron")
+    naam: str = Field(..., description="Name of the person mentioned")
+    geb: Optional[str] = Field(..., description="The birth date of the peron")
+    geb_plaats: str = Field(..., description="The birth place of the peron")
     gest: str = Field(..., description="The death date of the peron")
+    gest_plaats: str = Field(..., description="The place where this person has died")
+    extra_info: str = Field(..., description="Extra info about that specific person such as education and job.")
+
+
+class Echtgenote(BaseModel):
+    """Identifying information about a person."""
+
+    naam: str = Field(..., description="Name of the person mentioned")
+    geb: str = Field(..., description="The birth date of the peron")
+    geb_plaats: str = Field(..., description="The birth place of the peron")
+    gest: str = Field(..., description="The death date of the peron")
+    gest_plaats: str = Field(..., description="The place where this person has died")
+    marriage: str = Field(..., description="which marriage is this. ex: getr. 1")
+    extra_info: str = Field(..., description="Extra info about that specific person such as education and job.")
+
+
+class Ouder(BaseModel):
+    """Identifying information about a person's father or mother inlaw."""
+
+    naam: str = Field(..., description="Name of the person mentioned")
+    extra_info: str = Field(..., description="Extra info about that specific person such as birth/death date education and job.")
+
+
+class Persoon(BaseModel):
+    """Information about a person."""
+    naam: str = Field(..., description="Name of the person. each text can only contain one name")
+    geb: str = Field(..., description="The birth date of the peron")
+    geb_plaats: str = Field(..., description="The birth place of the peron")
+    gest: str = Field(..., description="The death date of the peron")
+    gest_plaats: str = Field(..., description="The place where this person has died")
     opleiding: List[str] = Field(..., description="Education information")
     loopbaan: List[str] = Field(..., description="Career information")
+    nevenfuncties: Optional[str] = Field(None, description="volunteer work information")
     bijzonderheden: List[str] = Field(..., description="Additional details")
-    echtgenote: Optional[str] = Field(None, description="Name of the spouse")
-    kinderen: List[Kinderen] = Field(None, description="Names of the children")
+    ouders: Optional[Ouder] = Field(None, description="Names of the parents")
+    ouders_vader: Optional[Ouder] = Field(None, description="Names of the grandparents")
+    ouders_moeder: Optional[Ouder] = Field(None, description="Names of the grandparents")
+    echtgenote: Optional[Echtgenote] = Field(None, description="Name of the spouse")
+    ouders_echtgenote: Optional[Ouder] = Field(None, description="Names of the father and mother in law")
+    kinderen: Optional[Kind] = Field(None, description="Names of the children")
     # kinderen: Sequence[str] = Field(None, description="Names of the children")
 
 
 # Extraction
-chain = create_extraction_chain_pydantic(pydantic_schema=PersonInfo, llm=chat)
+chain = create_extraction_chain_pydantic(pydantic_schema=Persoon, llm=chat)
 
 # Run
-text = """BONTIUS (BONDT, DE BONT) ,Gerardus(Geraert)
+text = """DECKERS (DEKKERS), Fredericus
 
-1.Geb. Rijswijk(Gld.) 7536
-Gest.lLeiden 15-09-1599
+1.Geb.’s Hertogenbosch 23-12-1648
+Gest.Leiden 03-11-1720
 
-20pleidings
--Les Adrianus Agrippa
+2.Opleiding:
 
-Schoonhoven,
+-Voorbereidend onderwijs ’s Hertogenbosch
+-Stud.Med. Leiden 05-12-1664
+-Doct.Med., Leiden 01-02-1668
 
-Delft ,Geldrop
--5tud.Med. Leuven
--Studiereis Italië
--Doct.Med., Padua
+3.Loopbaans:
 
-3eLoopbaans
+-Arts Leiden 1668
+-Hoogleraar practische Leiden 15-11-1694
+Geneeskunde 0:20-712-71694
 
--Arts Leiden
+-Hoogleraar collegium Leiden 18-05-1697
+practicum-medicum
 
--Hoogleraar Medicijnen en Leiden 17-07-1575
-vrije consten(wis- en
-sterrekunde )
+-Hmeritus: 177
 
--Onderwijs Kruid-en Ont- Leiden -09-1587
-leedkunde
+-Rector Magnificus Leiden 1700/01,1707/08,
 
--Toezicht Hortus Leiiden T0-70-1598
+1715/16
+4eNevenfonncties
 
--Rector Magnificus Leiden 1582-83,15909
+-Secretaris van de Senaa:t
+BeBijzonderheden:
 
-5 .Bijzonderheden:
+-Diss.De capitis dolore bij prof.Schacht
 
--Woont op Gerecht,hoek Lengesteeg.
+-Woont Oude Rijn(1670) ,Steenschuur(17017)
 
--Salaris: f300,-(1575),f400,-(1589),f500,-(1591),f600,-(1594).
--NVezijzinnig in religiosis,.
+-Salaris: f1600,- (än 1694)
 
-9.Echtgenote:
+-Hij is de derde voor de vacature Pitcairne} voor hem wezen
+Cyprianus(hoogleraar Franeker) en Brunner(lijfarts van de
+keurvorst van de Palts)het aanbod af.
 
-Jacoba Jansdr.,
-11.Kimderen:s
+6.Ouders:
+Hubertus Dekkers
 
-1 .Agnes
-2.Marie
+9.Echtgenotess
 
-3edan
-Begr.Rotterdam 18/25-04-1632°
-Arts en gemeenteontvanger Rotterdam
-Getr.Rotterdam 20-06-17599 Lideroy van Goedereede
+Maria Breyne(Brayns) (2,a)
+Gest.23-04-1700
+Getr.Leiden 16-05-1670
 
-4.JacoMb
-Geb. Leiden 1598
-Gest.Nederlands-Indië 30-117-1637
-Arts te Leiden,Inspecteur chirurgen Indi&
+Adriana van Aeckeren (2,29, a)
+Ged. Leiden 25-12-1649
 
-en Advocaat-Fiscaal Indië.
-Getr.Leiden 22-01-1676 Agneta Jansdr.,
+Gest.Leiden 26-12-1703
 
-5eReinier (zie hierna)
-Geb.Leiden 1576
-Gest.Leiden 12-06-1623(T79) of 13-06-1623(20)
-Hoogleraar Geneeskunde Leiden
-Ondt.1. Leiden 02-06-16017 Leonora Fabiusdr.
-Ondt.2.Leiden 02-04-1618 Clementia Willemsdr.-van der Aa
+Getr.l.Matteus Pollenaer,lid vroedschap Leiden op 13-12-1667
 
-6.eWMilLLem
-Gest.Leiden 16-710-1646
-Hoogleraar Rechten Leiden,Schout Leiden
-Ondt Leiden 05-06-1626 Maria van Dilsen
+Getr.2. 02-07-1670 Dirk Verhagen,notaris en schatmeester
 
-a) Bevolkingsregisteer 1581,GA Leiden
+van de prinselijke domeinen in Zoeterwoude
+
+Getre3.Leiden 24-07-1701
+10.Ouders Echtgenotes:
+
+Pieter Mattijsz.Braine ,koopman,geboren te Keulen
+Sarah de Koorn (1651-1700)
+
+dan Meindertsz.van Aeckeren
+
+Haesje Jansdr.van Rijn
+
+11.Kinderen:(allen uit eerste huwelijk)
+
+13arah
+Ged. Leiden 08-02-1677
+
+2.Hubertus
+Ged. Leriden 20-11-1672
+Gest.eLeiden 02-11-1720
+stadsarts: Leiden
+ondt.Leiden 22-08-1697 Johanna Jacoba Verhagen (dochter uit tweede
+huwelijk van zijn aanstaande stiefmoeder)
+
+3.dareh
+Ged. Leiden 08-05-1674
+
+4.Johanna
+
+Ged. Leiden 10-12-1675
+59eserrahn
+
+Ged. Leiden 70-05-1678
+6G.Meria
+
+Ged. Leiden 18-06-17680
+
+7.5Sarah
+Ged. Leiden 26-04-1682
+
+8.Jacoba
+
+Ged. Leiden 05-11-11683
+9.Sarah
+
+Ged. Leiden 07-04-1686
+
+T0.Johannes Hendecik
+Gest. 02-17:-11720
+Arts tes Hamburg:
+
+a) DTB GA Leiden
 """
 output = chain.run(text)
 print(output)
