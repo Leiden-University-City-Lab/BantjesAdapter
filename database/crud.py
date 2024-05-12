96424c7691198f7967b3b833668174b3dcf5f916
Z. Abedi <z.abedi@umail.leidenuniv.nl>
2024-05-12 19:34:17 +0200

    crud in progress

--- database/crud.py
+++ database/crud.py
@@ -1,88 +1,125 @@
-from sqlalchemy.sql import and_, or_, func
+from sqlalchemy import and_, or_, func, update
 from database import Session
-from . import models, schemas
+from .models import Person, Location, TypeOfPerson, Relation, TypeOfRelation, Education, Particularity, \
+    Career, TypeOfLocation
+from . import schemas
 
 session = Session()
 
 
 def get_person(family_name: str, first_name: str, birth_year: str, birth_place: str):
     # add table names we want to get back in the response
-    return session.query(models.Person, models.TypeOfPerson).join(
-        models.Location,
-        and_(models.Location.locationPersonID == models.Person.personPersonID,
-             models.Person.TypeOfPerson == 1
+    return session.query(Person,
+                         TypeOfPerson,
+                         Relation,
+                         TypeOfRelation,
+                         Location,
+                         TypeOfLocation,
+                         Particularity,
+                         Education,
+                         Career).join(
+        Location,
+        and_(Location.locationPersonID == Person.personPersonID,
+             Person.TypeOfPerson == 1
              )).outerjoin(
-        models.Relation,
-        models.Relation.FromPersonID == models.Person.personPersonID
+        Relation,
+        Relation.FromPersonID == Person.personPersonID
     ).outerjoin(
-        models.TypeOfRelation,
-        models.Relation.RelationID == models.TypeOfRelation.RelationID
+        TypeOfLocation,
+        Location.TypeOfLocation == TypeOfLocation.LocationID
     ).outerjoin(
-        models.Family,
-        models.Relation.ToFamilyID == models.Family.FamilyID
+        TypeOfRelation,
+        Relation.RelationID == TypeOfRelation.RelationID
     ).outerjoin(
-        models.Education,
-        models.Person.personPersonID == models.Education.personPersonID
+        Education,
+        Person.personPersonID == Education.personPersonID
     ).outerjoin(
-        models.Career,
-        models.Person.personPersonID == models.Career.personPersonID
+        Career,
+        Person.personPersonID == Career.personPersonID
     ).outerjoin(
-        models.TypeOfPerson,
-        models.Person.TypeOfPerson == models.TypeOfPerson.PersonID
+        TypeOfPerson,
+        Person.TypeOfPerson == TypeOfPerson.PersonID
     ).outerjoin(
-        models.Particularity,
-        models.Person.personPersonID == models.Particularity.personPersonID
+        Particularity,
+        Person.personPersonID == Particularity.personPersonID
     ).filter(
         or_(
             and_(
-                models.Person.FirstName.like(f'%{first_name}%'),
-                models.Person.LastName.like(f'%{family_name}%'),
+                Person.FirstName.like(f'%{first_name}%'),
+                Person.LastName.like(f'%{family_name}%'),
                 or_(
-                    func.extract('YEAR', models.Location.locationStartDate) == f'{birth_year}',
-                    models.Location.locationStartDate is None
+                    func.extract('YEAR', Location.locationStartDate) == f'{birth_year}',
+                    Location.locationStartDate is None
                 ),
                 or_(
-                    models.Location.City == f'{birth_place}',
-                    models.Location.City is None
+                    Location.City == f'{birth_place}',
+                    Location.City is None
                 )
             ),
             and_(
-                models.Person.LastName.like(f'%{family_name}%'),
-                func.extract('YEAR', models.Location.locationStartDate) == f'{birth_year}',
+                Person.LastName.like(f'%{family_name}%'),
+                func.extract('YEAR', Location.locationStartDate) == f'{birth_year}',
                 or_(
-                    models.Location.City == f'{birth_place}',
-                    models.Location.City is None
+                    Location.City == f'{birth_place}',
+                    Location.City is None
                 )
             ),
             and_(
-                models.Person.LastName.like(f'%{family_name}%'),
+                Person.LastName.like(f'%{family_name}%'),
                 or_(
-                    func.extract('YEAR', models.Location.locationStartDate) == f'{birth_year}',
-                    models.Location.locationStartDate is None
+                    func.extract('YEAR', Location.locationStartDate) == f'{birth_year}',
+                    Location.locationStartDate is None
                 ),
-                models.Location.City == f'{birth_place}'
+                Location.City == f'{birth_place}'
             )
         )
-    ).one()
+    ).all()
 
 
-def update_person():
-    return ''
+def update_person(person_ocr: schemas.Person, person_db: Person):
+
+    if person_db is None:
+        return None
+
+    person_db.Faculty = person_ocr.faculty
+    person_db.Rating = 2
+
+    session.add(person_db)
+    session.commit()
+    session.refresh(person_db)
+    return person_db
+
+
+def update_education(education_ocr: schemas.Education, education_db: Education, person_id: int):
+    if education_db is None:
+        return None
+
+    for education in education_ocr:
+        education_db.personPersonID = person_id
+        education_db.Subject = education.subject
+        education_db.Location = education.location
+        education_db.Date = education.date
+        education_db.Source = education.source
+
+        session.add(education_db)
+        session.commit()
+        session.refresh(education_db)
+
 
 # def get_birth_year():
 #     return session.query(
-#         func.extract('YEAR', models.Location.locationStartDate).label("Birth year"),
+#         func.extract('YEAR', Location.locationStartDate).label("Birth year"),
 #     ).join(
-#         models.Person,
-#         and_(models.Person.personPersonID == models.Location.locationPersonID, models.Person.type_of_person == 1)
+#         Person,
+#         and_(Person.personPersonID == Location.locationPersonID, Person.type_of_person == 1)
 #     ).filter(
-#         models.Location.TypeOfLocation == 1,
-#         models.Location.locationStartDate is not None
+#         Location.TypeOfLocation == 1,
+#         Location.locationStartDate is not None
 #     )
 
 
 def create_person(person: schemas.Person):
-    db_user = models.Person(FirstName=person.first_name,
+    db_user = Person(FirstName=person.first_name,
                             LastName=person.last_name,
                             FamilyName=person.last_name,
                             Affix=person.affix,
